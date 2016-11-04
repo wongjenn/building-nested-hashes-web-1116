2e27f74b2255365faa87a59f68c14936be4844ec
Jennifer Wong <wongjenn@yahoo.com>
2016-11-04 00:05:18 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -25,7 +25,7 @@ def bonus
   epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 
-puts epic_tragedy
+
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
